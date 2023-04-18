229ff2476742209b9f63ebfed20ed22b345f7799
xutong8 <2451836009@qq.com>
2023-04-18 16:08:36 +0800

    refractor: ProductBaseEdit

---
+++ src/components/DataManagement/ProductBaseEdit/index.tsx
@@ -0,0 +1,273 @@
+import React, { useState } from "react";
+import { Button, Input, InputNumber, Select } from "antd";
+import styles from "./index.module.less";
+import TextArea from "antd/es/input/TextArea";
+import { PlusCircleTwoTone } from "@ant-design/icons";
+import Checkbox, { CheckboxChangeEvent } from "antd/es/checkbox/Checkbox";
+import { close } from "@/assets";
+import { FilterCakeType, ProductType, RawMaterialType } from "../ProductEdit";
+import { IProductSeries } from "@/services/fetchProductSeries";
+import { IRawMaterial } from "@/services/fetchRawMaterials";
+import { IProduct } from "@/services/fetchProductById";
+import { IFilterCake } from "@/services/fetchFilterCakes";
+
+export interface IProductBaseEditProps {
+  product: ProductType;
+  setProduct: (product: ProductType) => void;
+  series: IProductSeries[];
+  rawMaterials: IRawMaterial[];
+  filterCakes: IFilterCake[];
+}
+
+const ProductBaseEdit: React.FC<IProductBaseEditProps> = (props) => {
+  const { product, setProduct, series, rawMaterials, filterCakes } = props;
+
+  // 选中的原料
+  const [selectedRawMaterial, setSelectedRawMaterial] =
+    useState<RawMaterialType>(null);
+  // 选中的滤饼
+  const [selectedFilterCake, setSelectedFilterCake] =
+    useState<FilterCakeType>(null);
+  // 原料投入量
+  const [rmAmount, setRMAmount] = useState<number>(0);
+  // 原料启用百分比
+  const [rmEnable, setRMEnable] = useState<boolean>(false);
+  // 滤饼投入量
+  const [fcAmount, setFCAmount] = useState<number>(0);
+  // 滤饼启用百分比
+  const [fcEnable, setFCEnable] = useState<boolean>(false);
+
+  // 添加原料关联
+  const handleRmAdd = () => {};
+
+  const relations = [
+    { name: "原料1", amount: 100 },
+    { name: "原料2", amount: 150 },
+  ];
+
+  const handleClose = () => {};
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        {/* 产品名称 */}
+        <div className={styles.base}>
+          <p className={styles.field}>产品名称：</p>
+          <Input
+            className={styles.input}
+            value={product?.productName ?? ""}
+            onChange={(event) => {
+              setProduct({
+                ...(product ?? ({} as IProduct)),
+                productName: event.target.value,
+              });
+            }}
+          />
+        </div>
+        {/* 产品编号 */}
+        <div className={styles.base}>
+          <p className={styles.field}>产品编号：</p>
+          <Input
+            className={styles.input}
+            value={product?.productIndex ?? ""}
+            onChange={(event) => {
+              setProduct({
+                ...(product ?? ({} as IProduct)),
+                productIndex: event.target.value,
+              });
+            }}
+          />
+        </div>
+        {/* 产品代码 */}
+        <div className={styles.base}>
+          <p className={styles.field}>产品代码：</p>
+          <Input
+            className={styles.input}
+            value={product?.productCode ?? ""}
+            onChange={(event) => {
+              setProduct({
+                ...(product ?? ({} as IProduct)),
+                productCode: event.target.value,
+              });
+            }}
+          />
+        </div>
+        {/* 产品系列 */}
+        <div className={styles.base}>
+          <p className={styles.field}>产品系列：</p>
+          <Select
+            className={styles.input}
+            value={product?.productSeriesName ?? ""}
+            options={series.map((item) => ({
+              value: item.productSeriesName,
+              label: item.productSeriesName,
+            }))}
+          />
+        </div>
+        {/* 产品颜色 */}
+        <div className={styles.base}>
+          <p className={styles.field}>产品颜色：</p>
+          <Input
+            className={styles.input}
+            value={product?.productColor ?? ""}
+            onChange={(event) => {
+              setProduct({
+                ...(product ?? ({} as IProduct)),
+                productColor: event.target.value,
+              });
+            }}
+          />
+        </div>
+        {/* TODO: 加工成本 */}
+        <div className={styles.base}>
+          <p className={styles.field}>加工成本：</p>
+          <Input className={styles.input} defaultValue={0} />
+        </div>
+        {/* TODO: 核算数量 */}
+        <div className={styles.base}>
+          <p className={styles.field}>核算数量：</p>
+          <Input className={styles.input} defaultValue={0} />
+        </div>
+        {/* TODO: 附加信息 */}
+        <div className={styles.base}>
+          <p className={styles.field}>附加信息：</p>
+          <TextArea className={styles.input} defaultValue={"请输入附加信息"} />
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.rm_relations}>
+          <div className={styles.title}>
+            <div className={styles.field}>原料关联</div>
+            <Button
+              className={styles.btn}
+              type="primary"
+              ghost
+              icon={<PlusCircleTwoTone />}
+              onClick={handleRmAdd}
+            >
+              新增
+            </Button>
+          </div>
+          <div className={styles.new_relation}>
+            <div className={styles.name}>
+              <p>原料名称：</p>
+              <Select
+                value={selectedRawMaterial?.rawMaterialName}
+                options={rawMaterials.map((rawMaterial) => ({
+                  value: rawMaterial?.rawMaterialName ?? "",
+                  label: rawMaterial?.rawMaterialName ?? "",
+                }))}
+                className={styles.select}
+                onChange={(rawMaterialName: string) => {
+                  const rawMaterial =
+                    rawMaterials.find(
+                      (rawMaterial) =>
+                        rawMaterial.rawMaterialName === rawMaterialName
+                    ) ?? null;
+                  setSelectedRawMaterial(rawMaterial);
+                }}
+              />
+            </div>
+            <div className={styles.amount}>
+              <p>原料用量：</p>
+              <InputNumber
+                value={rmAmount}
+                onChange={(value) => setRMAmount(value as number)}
+              />
+            </div>
+            <div className={styles.enable}>
+              <Checkbox
+                checked={rmEnable}
+                onChange={(e: CheckboxChangeEvent) => {
+                  setRMEnable(e.target.checked);
+                }}
+              >
+                启用百分比
+              </Checkbox>
+            </div>
+          </div>
+          <div className={styles.exist_relations}>
+            {relations.map((relation, index: number) => (
+              <div key={index} className={styles.exist_relation}>
+                <div className={styles.content}>
+                  <p>{relation.amount}</p>
+                  <p>{relation.name}</p>
+                </div>
+                <div className={styles.close}>
+                  <img src={close} alt="close icon" onClick={handleClose} />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={styles.fc_relations}>
+          <div className={styles.title}>
+            <div className={styles.field}>滤饼关联</div>
+            <Button
+              className={styles.btn}
+              type="primary"
+              ghost
+              icon={<PlusCircleTwoTone />}
+              onClick={handleRmAdd}
+            >
+              新增
+            </Button>
+          </div>
+          <div className={styles.new_relation}>
+            <div className={styles.name}>
+              <p>滤饼名称：</p>
+              <Select
+                value={selectedFilterCake?.filterCakeName}
+                options={filterCakes.map((filterCake) => ({
+                  value: filterCake?.filterCakeName ?? "",
+                  label: filterCake?.filterCakeName ?? "",
+                }))}
+                className={styles.select}
+                onChange={(filterCakeName: string) => {
+                  const filterCake =
+                    filterCakes.find(
+                      (filterCake) =>
+                        filterCake.filterCakeName === filterCakeName
+                    ) ?? null;
+                  setSelectedFilterCake(filterCake);
+                }}
+              />
+            </div>
+            <div className={styles.amount}>
+              <p>滤饼用量：</p>
+              <InputNumber
+                value={fcAmount}
+                onChange={(value) => setFCAmount(value as number)}
+              />
+            </div>
+            <div className={styles.enable}>
+              <Checkbox
+                checked={fcEnable}
+                onChange={(e: CheckboxChangeEvent) =>
+                  setFCEnable(e.target.value)
+                }
+              >
+                启用百分比
+              </Checkbox>
+            </div>
+          </div>
+          <div className={styles.exist_relations}>
+            {relations.map((relation, index: number) => (
+              <div key={index} className={styles.exist_relation}>
+                <div className={styles.content}>
+                  <p>{relation.amount}</p>
+                  <p>{relation.name}</p>
+                </div>
+                <div className={styles.close}>
+                  <img src={close} alt="close icon" onClick={handleClose} />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductBaseEdit;
--- src/components/DataManagement/ProductEdit/index.tsx
+++ src/components/DataManagement/ProductEdit/index.tsx
@@ -1,15 +1,12 @@
 import { IProduct } from "@/services/fetchProductById";
-import { Button, Input, InputNumber, Modal, Select } from "antd";
+import { Modal } from "antd";
 import React, { forwardRef, useState } from "react";
 import { useImperativeHandle, ForwardedRef } from "react";
 import styles from "./index.module.less";
 import { IProductSeries } from "@/services/fetchProductSeries";
-import TextArea from "antd/es/input/TextArea";
-import { PlusCircleTwoTone } from "@ant-design/icons";
 import { IRawMaterial } from "@/services/fetchRawMaterials";
-import Checkbox, { CheckboxChangeEvent } from "antd/es/checkbox/Checkbox";
-import { close } from "@/assets";
 import { IFilterCake } from "@/services/fetchFilterCakes";
+import ProductBaseEdit from "../ProductBaseEdit";
 
 export interface IProductEditProps {}
 
@@ -37,41 +34,15 @@ const ProductEdit = (
   const [rawMaterials, setRawMaterials] = useState<IRawMaterial[]>([]);
   // 滤饼名称
   const [filterCakes, setFilterCakes] = useState<IFilterCake[]>([]);
-  // 选中的原料
-  const [selectedRawMaterial, setSelectedRawMaterial] =
-    useState<RawMaterialType>(null);
-  // 选中的滤饼
-  const [selectedFilterCake, setSelectedFilterCake] =
-    useState<FilterCakeType>(null);
-  // 原料投入量
-  const [rmAmount, setRMAmount] = useState<number>(0);
-  // 原料启用百分比
-  const [rmEnable, setRMEnable] = useState<boolean>(false);
-  // 滤饼投入量
-  const [fcAmount, setFCAmount] = useState<number>(0);
-  // 滤饼启用百分比
-  const [fcEnable, setFCEnable] = useState<boolean>(false);
 
   // 点击确认
   const handleOk = () => {
-    setSelectedRawMaterial(null);
-    setRMAmount(0);
-    setRMEnable(false);
-    setSelectedFilterCake(null);
-    setFCAmount(0);
-    setFCEnable(false);
     setShowModal(false);
     // TODO：添加修改产品的逻辑
   };
 
   // 点击取消
   const handleCancel = () => {
-    setSelectedRawMaterial(null);
-    setRMAmount(0);
-    setRMEnable(false);
-    setSelectedFilterCake(null);
-    setFCAmount(0);
-    setFCEnable(false);
     setShowModal(false);
   };
 
@@ -83,20 +54,6 @@ const ProductEdit = (
     setFilterCakes,
   }));
 
-  // 添加原料关联
-  const handleRmAdd = () => {};
-
-  const relations = [
-    { name: "原料1", amount: 100 },
-    { name: "原料2", amount: 150 },
-    { name: "原料1", amount: 100 },
-    { name: "原料2", amount: 150 },
-    { name: "原料1", amount: 100 },
-    { name: "原料2", amount: 150 },
-  ];
-
-  const handleClose = () => {};
-
   return (
     <Modal
       title="产品编辑"
@@ -107,228 +64,13 @@ const ProductEdit = (
       cancelText="取消"
       width={1000}
     >
-      <div className={styles.container}>
-        <div className={styles.left}>
-          {/* 产品名称 */}
-          <div className={styles.base}>
-            <p className={styles.field}>产品名称：</p>
-            <Input
-              className={styles.input}
-              value={product?.productName ?? ""}
-              onChange={(event) => {
-                setProduct({
-                  ...(product ?? ({} as IProduct)),
-                  productName: event.target.value,
-                });
-              }}
-            />
-          </div>
-          {/* 产品编号 */}
-          <div className={styles.base}>
-            <p className={styles.field}>产品编号：</p>
-            <Input
-              className={styles.input}
-              value={product?.productIndex ?? ""}
-              onChange={(event) => {
-                setProduct({
-                  ...(product ?? ({} as IProduct)),
-                  productIndex: event.target.value,
-                });
-              }}
-            />
-          </div>
-          {/* 产品代码 */}
-          <div className={styles.base}>
-            <p className={styles.field}>产品代码：</p>
-            <Input
-              className={styles.input}
-              value={product?.productCode ?? ""}
-              onChange={(event) => {
-                setProduct({
-                  ...(product ?? ({} as IProduct)),
-                  productCode: event.target.value,
-                });
-              }}
-            />
-          </div>
-          {/* 产品系列 */}
-          <div className={styles.base}>
-            <p className={styles.field}>产品系列：</p>
-            <Select
-              className={styles.input}
-              value={product?.productSeriesName ?? ""}
-              options={series.map((item) => ({
-                value: item.productSeriesName,
-                label: item.productSeriesName,
-              }))}
-            />
-          </div>
-          {/* 产品颜色 */}
-          <div className={styles.base}>
-            <p className={styles.field}>产品颜色：</p>
-            <Input
-              className={styles.input}
-              value={product?.productColor ?? ""}
-              onChange={(event) => {
-                setProduct({
-                  ...(product ?? ({} as IProduct)),
-                  productColor: event.target.value,
-                });
-              }}
-            />
-          </div>
-          {/* TODO: 加工成本 */}
-          <div className={styles.base}>
-            <p className={styles.field}>加工成本：</p>
-            <Input className={styles.input} defaultValue={0} />
-          </div>
-          {/* TODO: 核算数量 */}
-          <div className={styles.base}>
-            <p className={styles.field}>核算数量：</p>
-            <Input className={styles.input} defaultValue={0} />
-          </div>
-          {/* TODO: 附加信息 */}
-          <div className={styles.base}>
-            <p className={styles.field}>附加信息：</p>
-            <TextArea
-              className={styles.input}
-              defaultValue={"请输入附加信息"}
-            />
-          </div>
-        </div>
-        <div className={styles.right}>
-          <div className={styles.rm_relations}>
-            <div className={styles.title}>
-              <div className={styles.field}>原料关联</div>
-              <Button
-                className={styles.btn}
-                type="primary"
-                ghost
-                icon={<PlusCircleTwoTone />}
-                onClick={handleRmAdd}
-              >
-                新增
-              </Button>
-            </div>
-            <div className={styles.new_relation}>
-              <div className={styles.name}>
-                <p>原料名称：</p>
-                <Select
-                  value={selectedRawMaterial?.rawMaterialName}
-                  options={rawMaterials.map((rawMaterial) => ({
-                    value: rawMaterial?.rawMaterialName ?? "",
-                    label: rawMaterial?.rawMaterialName ?? "",
-                  }))}
-                  className={styles.select}
-                  onChange={(rawMaterialName: string) => {
-                    const rawMaterial =
-                      rawMaterials.find(
-                        (rawMaterial) =>
-                          rawMaterial.rawMaterialName === rawMaterialName
-                      ) ?? null;
-                    setSelectedRawMaterial(rawMaterial);
-                  }}
-                />
-              </div>
-              <div className={styles.amount}>
-                <p>原料用量：</p>
-                <InputNumber
-                  value={rmAmount}
-                  onChange={(value) => setRMAmount(value as number)}
-                />
-              </div>
-              <div className={styles.enable}>
-                <Checkbox
-                  checked={rmEnable}
-                  onChange={(e: CheckboxChangeEvent) => {
-                    setRMEnable(e.target.checked);
-                  }}
-                >
-                  启用百分比
-                </Checkbox>
-              </div>
-            </div>
-            <div className={styles.exist_relations}>
-              {relations.map((relation, index: number) => (
-                <div key={index} className={styles.exist_relation}>
-                  <div className={styles.content}>
-                    <p>{relation.amount}</p>
-                    <p>{relation.name}</p>
-                  </div>
-                  <div className={styles.close}>
-                    <img src={close} alt="close icon" onClick={handleClose} />
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className={styles.fc_relations}>
-            <div className={styles.title}>
-              <div className={styles.field}>滤饼关联</div>
-              <Button
-                className={styles.btn}
-                type="primary"
-                ghost
-                icon={<PlusCircleTwoTone />}
-                onClick={handleRmAdd}
-              >
-                新增
-              </Button>
-            </div>
-            <div className={styles.new_relation}>
-              <div className={styles.name}>
-                <p>滤饼名称：</p>
-                <Select
-                  value={selectedFilterCake?.filterCakeName}
-                  options={filterCakes.map((filterCake) => ({
-                    value: filterCake?.filterCakeName ?? "",
-                    label: filterCake?.filterCakeName ?? "",
-                  }))}
-                  className={styles.select}
-                  onChange={(filterCakeName: string) => {
-                    const filterCake =
-                      filterCakes.find(
-                        (filterCake) =>
-                          filterCake.filterCakeName === filterCakeName
-                      ) ?? null;
-                    setSelectedFilterCake(filterCake);
-                  }}
-                />
-              </div>
-              <div className={styles.amount}>
-                <p>滤饼用量：</p>
-                <InputNumber
-                  value={fcAmount}
-                  onChange={(value) => setFCAmount(value as number)}
-                />
-              </div>
-              <div className={styles.enable}>
-                <Checkbox
-                  checked={fcEnable}
-                  onChange={(e: CheckboxChangeEvent) =>
-                    setFCEnable(e.target.value)
-                  }
-                >
-                  启用百分比
-                </Checkbox>
-              </div>
-            </div>
-            <div className={styles.exist_relations}>
-              {relations.map((relation, index: number) => (
-                <div key={index} className={styles.exist_relation}>
-                  <div className={styles.content}>
-                    <p>{relation.amount}</p>
-                    <p>{relation.name}</p>
-                  </div>
-                  <div className={styles.close}>
-                    <img src={close} alt="close icon" onClick={handleClose} />
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
+      <ProductBaseEdit
+        product={product}
+        setProduct={setProduct}
+        series={series}
+        rawMaterials={rawMaterials}
+        filterCakes={filterCakes}
+      />
     </Modal>
   );
 };
